2b998ddb9e0364bdb7ddd4ec854acedddcc70f53
mohayraquibo <62005427+mohayraquibo@users.noreply.github.com>
2020-03-10 16:33:55 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_SERVER', 'it330phpserver.mysql.database.azure.com');
+define('DB_USERNAME', 'MohayraQuibo@it330phpserver');
+define('DB_PASSWORD', 'kalasahantakaw_0926');
+define('DB_NAME', 'it330db');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
